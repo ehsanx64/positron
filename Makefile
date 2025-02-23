c888d48c00c12900e2463ec1e233f450a1e4bc91
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-02-23 20:38:56 +0330

    feat: add viper package and config file

--- Makefile
+++ Makefile
@@ -13,12 +13,16 @@ dev:
 	air
 
 tidy:
+	GOPROXY=https://goproxy.io,direct
 	go mod tidy -v
 
 deps:
+	GOPROXY=https://goproxy.io,direct
 	go get github.com/labstack/echo/v4
+	go get github.com/spf13/viper
 	go get -u gorm.io/gorm
 	go get -u gorm.io/driver/sqlite
 
 dev-deps:
+	GOPROXY=https://goproxy.io,direct
 	go install github.com/air-verse/air@latest
--- cmd/positron/main.go
+++ cmd/positron/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/viper"
 
 	userHttpHandler "github.com/ehsanx64/positron/internal/domain/user/delivery/http"
-	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Println("Hello world!!!")
 	e := echo.New()
 
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Fatal("failed to load the config file")
+			// Config file not found; ignore error if desired
+		} else {
+			// Config file was found but another error was produced
+		}
+	}
+
+	// Config file found and successfully parsed
+
 	userHttpHandler.NewUserHTTPHandler(e)
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(viper.Get("app.port").(string)))
 }
